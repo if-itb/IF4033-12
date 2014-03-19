1765f1687721e85d3b0b63b297ba7d1f50456d1a
Gabrielle Wicesawati Poerwawinata <gabybongbong@gmail.com>
2014-03-19 18:47:08 +0700

    html

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html> 
+<body>
+<form action="registration.php" method="post">
+Username: <input type="text" name="name"><br>
+Password: <input type="text" name="email"><br>
+<input type="submit" value="Submit"> 
+<input type="submit" value="Forget Password">
+</form>
+
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html> 
+<body>
+<table width="400" border="0" align="center" cellpadding="3" cellspacing="1">
+<tr>
+<td><strong>Upload File </strong></td>
+</tr>
+</table>
+<table width="400" border="0" align="center" cellpadding="0" cellspacing="1">
+<tr>
+<td><form name="form1" method="post" action="send_contact.php">
+<table width="200%" border="0" cellspacing="1" cellpadding="3">
+<tr>
+<td width="25%">Dekripsi</td>
+<td width="2%">:</td>
+<td width="82%"><input name="subject" type="text" id="subject" size="50"></td>
+</tr>
+<tr>
+<td width="25%">Upload Photo</td>
+<td>:</td>
+<td><input name="customer_photo" type="file" id="customer_mail" size="50"></td>
+<td>:</td>
+<td></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type="submit" name="Submit" value="Submit"> <input type="reset" name="Submit2" value="Reset"></td>
+</tr>
+</table>
+</form>
+</td>
+</tr>
+</table>
+</body>
+</html>
