8baeeb28c0239eb2a7cbd917b8cc97158543f702
aknes <damanik.au@gmail.com>
2014-03-21 06:36:42 +0700

    mengganti input name dengan yang lebih sederhana

--- index.php
+++ index.php
@@ -9,15 +9,15 @@
 		var uname = encodeURI(document.getElementById('uname_login').value);
 		var passw = encodeURI(document.getElementById('password_login').value);
 		nocache = Math.random();	
-		http.open('get', 'php/login.php?username='+uname+'&password='+passw+'&nocache = '+nocache);
 		http.onreadystatechange = loginReply;
+		http.open('get', 'php/login.php?username='+uname+'&password='+passw+'&nocache = '+nocache);
 		http.send(null);
 	}
 </script>
 <b>Login</b>
 <form id="login_form" action="javascript:login()" onsubmit="login()" method="post">
-Username: <input type="text" name="uname_login"><br>
-Password: <input type="text" name="password_login"><br>
+Username: <input type="text" name="myusername"><br>
+Password: <input type="text" name="mypassword"><br>
 <input type="submit" value="Login" name="Submit"> 
 
 </form>
