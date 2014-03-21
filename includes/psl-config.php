f7436862d68d4ba82c3035165e34c29faae82e1a
aknes <damanik.au@gmail.com>
2014-03-21 15:11:36 +0700

    konfigurasi db

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -1,12 +1,12 @@
 <?php
 
 define("HOST", "localhost");     
-define("USER", "root");    // The database username. 
-define("PASSWORD", "");    // The database password. 
-define("DATABASE", "kpi_db");    // The database name.
+define("USER", "root");    //database username. 
+define("PASSWORD", "");    //database password. 
+define("DATABASE", "kpi_db");    //database name.
  
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
 
-define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+define("SECURE", FALSE);
 ?>
