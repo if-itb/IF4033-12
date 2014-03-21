16e6b936da793f11147f74d7a7a58df86589c095
aknes <damanik.au@gmail.com>
2014-03-21 15:03:31 +0700

    pesan kecil

--- protected_page.php
+++ protected_page.php
@@ -6,6 +6,7 @@ sec_session_start();
 
 if(login_check($mysqli) == true) {
         echo 'You are in.';
+		// Masukin file-upload disini
 } else { 
         echo 'You are not authorized to access this page, please login.';
 }
